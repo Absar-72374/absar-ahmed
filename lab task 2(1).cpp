179919ba8ba3f03bf7c8a54c1e521f40acdbbac4
Absar-72374 <kibsar457@gmail.com>
2025-12-31 12:38:00 +0500

    Add files via upload

---
+++ lab task 2(1).cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{ 
+    cout<<"Absar Ahmed";
+    cout<<"Sap Id:72374";
+    int length,width,height;
+    cout<<"Enter the length of the cube:"<<endl;
+    cin>>length;
+    cout<<"Enter the width of the cube:"<<endl;
+    cin>>width;
+    cout<<"Enter the height of the cube:"<<endl;
+    cin>>height;
+    int Volume = length*width*height;
+    cout<<"The Volume of the cube is:"<<Volume<<endl;
+    return 0;
+}
---
+++ lab task 2(3).cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Absar Ahmed";
+    cout<<"Sap Id:72374";
+    int F;
+    cout<<"Enter the temperature in Fahrenheit:";
+    cin>>F;
+    int C = 5/9(F-32);
+    cout<<"Temperature in Celsius:"<<C<<endl;
+    return 0;
+}
+    
